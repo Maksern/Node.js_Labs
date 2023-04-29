96767e8d39bcc4d6e916861ec48de19aa2d14f41
Maksern <maks.men9922@gmail.com>
2023-04-29 17:29:28 +0300

    add toUpper_FirstLetter_ofWords function

--- experiments.js
+++ experiments.js
@@ -1,1 +1,16 @@
-console.log("Hello world");
+let message = "Hello, my miamucho";
+
+function toUpper_FirstLetter_ofWords(message) {
+  let messageNew = "";
+  message.split(" ").map((word) => {
+    let upperWord = word.replace(
+      /\b\w/,
+      String(word.match(/\b\w/)).toUpperCase()
+    );
+    messageNew += upperWord + " ";
+  });
+
+  return messageNew;
+}
+
+console.log(toUpper_FirstLetter_ofWords(message));
