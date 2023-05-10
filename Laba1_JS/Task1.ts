827b8a951b9dc7cea12511376fb5055733d792e0
Maksern <maks.men9922@gmail.com>
2023-05-10 17:07:54 +0300

    Change files to typescript

---
+++ Laba1_JS/Task1.ts
@@ -0,0 +1,11 @@
+// function add(a){
+//       return function(b){
+//           if(!b ){
+//               return a;
+//           } else{
+//               return add(a + b);
+//           }
+//       }
+//   }
+//   var res = add(2)(5)(7)(1)(6)(5)(11)();
+//   console.log("Result - " + res);
