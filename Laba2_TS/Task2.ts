4bc89d2fd9049b733f3594356a85e841cb088936
Maksern <maks.men9922@gmail.com>
2023-05-10 17:17:05 +0300

    Rewrite task2 with typescript

--- Laba2_TS/Task2.ts
+++ Laba2_TS/Task2.ts
@@ -1,4 +1,4 @@
-function AnagramChecker(str1: string , str2: string){
+function AnagramChecker(str1: string , str2: string): boolean{
     if(str1.length != str2.length){
         return false
     } else {
